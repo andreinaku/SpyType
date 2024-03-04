ea12426fdfe262bcd725a8a640323b182f9feda0
Andrei Nacu <andrei@Andreis-MacBook-Air.local>
2024-03-04 08:20:59 +0200

    test cases to define future needs for spec apply

--- unit_tests/test_basetype.py
+++ unit_tests/test_basetype.py
@@ -20,3 +20,24 @@ class BasetypeTestCases(unittest.TestCase):
         result = apply_spec(state, spec, testmode=True)
         result2 = apply_spec(result, spec, testmode=True)
         self.assertEqual(result, result2)
+
+    def test_apply_spec_3(self):
+        state = Translator.translate_state(r'(a:int+float /\ b:int+float')
+        spec = Translator.translate_state(r'(a:T_a /\ b:T_b)')
+        result = apply_spec(state, spec, testmode=True)
+        expected_result = Translator.translate_state(r'(a:int+float /\ b:int+float')
+        self.assertEqual(result, expected_result)
+
+    def test_apply_spec_4(self):
+        state = Translator.translate_state(r'(a:int+float /\ b:int+float)')
+        spec = Translator.translate_state(r'(a:T_a /\ b:float)')
+        result = apply_spec(state, spec, testmode=True)
+        expected_result = Translator.translate_state(r'(a:int+float /\ b:float)')
+        self.assertEqual(result, expected_result)
+
+    def test_apply_spec_5(self):
+        state = Translator.translate_state(r'(a:list<int> /\ b:T_b)')
+        spec = Translator.translate_state(r'(a:list<T_1> /\ b:int)')
+        result = apply_spec(state, spec, testmode=True)
+        expected_result = Translator.translate_state(r'(a:list<int> /\ b:T_b) ^ ((T_b <= int \/ int <= T_b))')
+        self.assertEqual(result, expected_result)
