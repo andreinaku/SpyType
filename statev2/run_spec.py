a2bebd8887c4e16f17886fbd9beefc22454968a1
Andrei Nacu <andrei@Andreis-MacBook-Air-15.local>
2024-03-14 19:17:16 +0200

    apply specifications for expressions

--- statev2/run_spec.py
+++ statev2/run_spec.py
@@ -16,17 +16,37 @@ def get_constraints_list(state_set: StateSet) -> list[AndConstraints]:
     return ll
 
 
+def output_expressions(expr_list: list[str], str_state_set: str):
+    current = Translator.translate_state_set(str_state_set)
+    with open('out_constraints.txt', 'a') as f:
+        for expr in expr_list:
+            f.write(f'{'-' * (len(expr) + 4)}\n| {expr} |\n{'-' * (len(expr) + 4)}\n')
+            applied = get_states(current, expr)
+            print_str = ''
+            for constraint in get_constraints_list(applied):
+                retstr = str(constraint)
+                for eq_in, eq_out in type_equiv.items():
+                    retstr = retstr.replace(eq_in, eq_out)
+                print_str += f'({retstr}) \\/ \n'
+            f.write(print_str[:-5] + '\n\n')
+
+    with open('out_states.txt', 'a') as f:
+        for expr in expr_list:
+            f.write(f'{'-' * (len(expr) + 4)}\n| {expr} |\n{'-' * (len(expr) + 4)}\n')
+            applied = get_states(current, expr)
+            print_str = ''
+            for state in applied:
+                retstr = str(state)
+                for eq_in, eq_out in type_equiv.items():
+                    retstr = retstr.replace(eq_in, eq_out)
+                print_str += f'({retstr}) \\/ \n'
+            f.write(print_str[:-5] + '\n\n')
+
+
 if __name__ == "__main__":
-    simple_expressions = [
-        'a+b', 'a/b', 'a>>b', 'a*b', 'a-b'
-    ]
-    current = Translator.translate_state_set(r'(a:int+float /\ b:int+float+str)')
-    for simple_expr in simple_expressions:
-        print(f'{simple_expr}:\n')
-        applied = get_states(current, simple_expr)
-        for constraint in get_constraints_list(applied):
-            retstr = str(constraint)
-            for eq_in, eq_out in type_equiv.items():
-                retstr = retstr.replace(eq_in, eq_out)
-            print(retstr)
-        print('\n')
+    start_set = r'(a:int+float /\ b:int+float+str)'
+    simple_expr = ['a+b', 'a/b', 'a>>b', 'a*b', 'a-b']
+    expr_1 = ['(a+b)/c']
+    # output_expressions(simple_expr, start_set)
+    start_set = r'(a:int+float /\ b:int+float+str /\ c:str)'
+    output_expressions(expr_1, start_set)
