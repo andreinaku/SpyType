3c8e6cf388dc98d399ccec102a46d8bb15e0ebe0
Andrei Nacu <andreinaku@gmail.com>
2024-05-08 15:35:07 +0300

    wip call func arg match

--- statev2/transfer.py
+++ statev2/transfer.py
@@ -5,6 +5,139 @@ from united_specs import op_equiv, unitedspecs
 from statev2.Translator import Translator
 
 
+POSONLY_MARKER = '__po_'
+VARARG_MARKER = '__va_'
+KEYWORDONLY_MARKER = '__ko_'
+KWARG_MARKER = '__kw_'
+MARKERS = [POSONLY_MARKER, VARARG_MARKER, KEYWORDONLY_MARKER, KWARG_MARKER]
+RETURN_VARNAME = 'return'
+
+
+class ArgumentMismatchError(Exception):
+    pass
+
+
+# class SpecNotFoundError(Exception):
+#     pass
+
+class FunctionInstance:
+    def __init__(self, call_node: ast.Call, current_state: State, spec: FuncSpec):
+        self.arglink = dict()
+        self.current_state = deepcopy(current_state)
+        self.spec = deepcopy(spec)
+        self.pos_call_list = []
+        self.keyword_call_dict = {}
+        self.call_node = call_node
+        for node in self.call_node.args:
+            self.pos_call_list.append(astor.to_source(node).strip())
+        for node in self.call_node.keywords:
+            self.keyword_call_dict[node.arg] = node.value
+        self.arg_list = deepcopy(list(self.spec.in_state.assignment))
+        # self.arg_list.remove(RETURN_VARNAME)
+        self._param_link()
+
+    def _param_link(self):
+        if (len(self.pos_call_list) > 0 or len(self.keyword_call_dict) > 0) and len(self.arg_list) == 0:
+            raise ArgumentMismatchError('Too many arguments provided')
+        if (len(self.pos_call_list) == 0 and len(self.keyword_call_dict) == 0) and len(self.arg_list) > 0:
+            raise ArgumentMismatchError('Too few arguments provided')
+        if len(self.arg_list) == 0 and len(self.pos_call_list) == 0 and len(self.keyword_call_dict) == 0:
+            return
+        param: str = self.arg_list.pop(0)
+        if param.startswith(POSONLY_MARKER):
+            if len(self.pos_call_list) == 0:
+                raise ArgumentMismatchError('Too few positional arguments provided')
+            call_entry = self.pos_call_list.pop(0)
+            self.arglink[param] = call_entry
+            self._param_link()
+            # f(va_index+1)
+        elif param.startswith(VARARG_MARKER):
+            while len(self.pos_call_list) > 0:
+                call_entry = self.pos_call_list.pop(0)
+                if param not in self.arglink:
+                    self.arglink[param] = (call_entry,)
+                else:
+                    self.arglink[param] = self.arglink[param] + (call_entry,)
+            if param not in self.arglink:
+                raise ArgumentMismatchError(f'Variable argument {param} is not populated')
+            self._param_link()
+        elif va_var.startswith(KEYWORDONLY_MARKER):
+            if len(self.keyword_call_dict) == 0:
+                raise ArgumentMismatchError('Too few keyword arguments provided')
+            varname = va_var[len(KEYWORDONLY_MARKER):]
+            if varname not in self.keyword_call_dict:
+                raise ArgumentMismatchError(f'Keyword argument {varname} is not provided')
+            self.arglink[va_var] = astor.to_source(self.keyword_call_dict.pop(varname)).strip()
+            self._param_link()
+        elif va_var.startswith(KWARG_MARKER):
+            if len(self.keyword_call_dict) == 0:
+                raise ArgumentMismatchError(f'No more keyword arguments for {va_var}')
+            keyword_list = list(self.keyword_call_dict)
+            while len(self.keyword_call_dict) > 0:
+                ko_key = keyword_list.pop(0)
+                call_entry = astor.to_source(self.keyword_call_dict.pop(ko_key)).strip()
+                if va_var not in self.arglink:
+                    self.arglink[va_var] = {ko_key: call_entry}
+                else:
+                    self.arglink[va_var].update({ko_key: call_entry})
+            self._param_link()
+        else:
+            if len(self.pos_call_list) > 0:
+                call_entry = self.pos_call_list.pop(0)
+            elif len(self.keyword_call_dict) > 0:
+                keyword_list = list(self.keyword_call_dict)
+                ko_key = keyword_list.pop(0)
+                call_entry = astor.to_source(self.keyword_call_dict.pop(ko_key)).strip()
+            else:
+                raise ArgumentMismatchError(f'No more arguments for {va_var}')
+            self.arglink[va_var] = call_entry
+            self._param_link()
+
+    def get_param_link(self):
+        return self.arglink
+
+    def instantiate_function(self):
+        new_as = AbsState()
+        new_as.va = VarAssign()
+        new_as.tc = TypeConstraint()
+        var_entries = []
+        repl = dict()
+        for spec_var, state_var in self.arglink.items():
+            if isinstance(state_var, str):
+                new_as.va[state_var] = self.spec_state.va[spec_var]
+                repl[self.spec_state.va[spec_var]] = self.current_state.va[state_var]
+            elif isinstance(state_var, tuple):
+                new_te = TypeExpression()
+                spec_vartype = self.spec_state.va[spec_var]
+                new_pytype = PyType(tuple)
+                for sv in state_var:
+                    new_as.va[sv] = self.current_state.va[sv]
+                    new_pytype.keys.add(self.current_state.va[sv])
+                new_te.add(new_pytype)
+                var_entries.append((spec_vartype, hset({new_te})))
+            elif isinstance(state_var, dict):
+                new_te = TypeExpression()
+                spec_vartype = self.spec_state.va[spec_var]
+                new_pytype = PyType(dict)
+                new_pytype.keys.add(PyType(str))
+                for k, sv in state_var.items():
+                    new_as.va[sv] = self.current_state.va[sv]
+                    new_pytype.values.add(self.current_state.va[sv])
+                new_te.add(new_pytype)
+                var_entries.append((spec_vartype, hset({new_te})))
+            else:
+                raise RuntimeError(f'Unexpected type of {state_var}: {type(state_var)}')
+        new_as.tc = deepcopy(self.spec_state.tc)
+        for ctx in new_as.tc:
+            for var_entry in var_entries:
+                if var_entry[0] not in ctx:
+                    raise RuntimeError(f'No variable entry in specs for {var_entry[0]}')
+                ctx[var_entry[0]] |= var_entry[1]
+        new_as = new_as.vartype_replace_by_dict(repl)
+        new_as.va[RETURN_VARNAME] = self.spec_state.va[RETURN_VARNAME]
+        return new_as
+
+
 def state_apply_spec(state: State, spec: State, testmode=False) -> State:
     new_state = State()
     new_state.gen_id = state.gen_id
