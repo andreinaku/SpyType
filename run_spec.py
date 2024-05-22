1288e9b5d0413dc607cc8dd25d98f52ecc102404
Andrei Nacu <andreinaku@gmail.com>
2024-05-22 08:17:57 +0300

    refactor for maude_gen

--- run_spec.py
+++ run_spec.py
@@ -1,84 +1,17 @@
-import os.path
-import os
-import ast
-import maude
-from statev2.maude_gen import mod_generator
-from statev2.transfer import *
-from united_specs import unitedspecs, op_equiv
-
-
-def get_states(state_set: StateSet, expr: str) -> StateSet:
-    tree = ast.parse(expr)
-    tf = TransferFunc(state_set)
-    tf.visit(tree)
-    return tf.state_set
-
-
-def get_constraints_list(state_set: StateSet) -> list[AndConstraints]:
-    ll = []
-    for state in state_set:
-        ll.append(state.constraints)
-    return ll
-
-
-def get_constraints_string_from_states(state_set: StateSet) -> str:
-    print_str = ''
-    constr_list = get_constraints_list(state_set)
-    for constraint in constr_list:
-        retstr = str(constraint)
-        # for eq_in, eq_out in type_equiv.items():
-        #     retstr = retstr.replace(eq_in, eq_out)
-        print_str += f'({retstr}) \\/ {os.linesep}'
-    print_str = print_str[:(-4 - len(os.linesep))]
-    return print_str
-
-
-def output_expressions(expr_list: list[str], str_state_set: str,
-                       outc_file: str = 'out_constraints.txt', outs_file: str = 'out_states.txt'):
-    current = Translator.translate_state_set(str_state_set)
-    if os.path.exists(outc_file):
-        os.remove(outc_file)
-    if os.path.exists(outs_file):
-        os.remove(outs_file)
-    with open(outc_file, 'a') as f:
-        for expr in expr_list:
-            f.write(f'{'-' * (len(expr) + 4)}\n| {expr} |\n{'-' * (len(expr) + 4)}\n')
-            applied = get_states(current, expr)
-            print_str = get_constraints_string_from_states(applied)
-            f.write(print_str + '\n\n')
-    with open(outs_file, 'a') as f:
-        for expr in expr_list:
-            f.write(f'{'-' * (len(expr) + 4)}\n| {expr} |\n{'-' * (len(expr) + 4)}\n')
-            applied = get_states(current, expr)
-            print_str = ''
-            for state in applied:
-                retstr = str(state)
-                # for eq_in, eq_out in type_equiv.items():
-                #     retstr = retstr.replace(eq_in, eq_out)
-                print_str += f'({retstr}) \\/ \n'
-            f.write(print_str[:-5] + '\n\n')
-
-
-def dump_to_maude(expr: str, str_state_set: str, dump=False) -> str:
-    current = Translator.translate_state_set(str_state_set)
-    applied = get_states(current, expr)
-    applied = applied.replace_superclasses()
-    c_value = get_constraints_string_from_states(applied)
-    maude_input = mod_generator('tempmod', c_value, dump_to_file=dump)
-    return maude_input
+from statev2.maude_gen import *
 
 
 if __name__ == "__main__":
-    #start_set = r'(a:int + float /\ b:int + float + str)'
-    # simple_expr = ['a+b', 'a/b', 'a>>b', 'a*b', 'a-b']
+    start_set = r'(a:int + float /\ b:int + float + str)'
+    simple_expr = ['a+b', 'a/b', 'a>>b', 'a*b', 'a-b']
     # simple_expr = ['(a+b)/c']
     # simple_expr = ['len(a)']
     # output_expressions(simple_expr, start_set)
     # start_set = r'(a:int+float /\ b:int+float+str /\ c:str)'
     # output_expressions(expr_1, start_set)
-    start_set = r'(c:Tc + list< float >)'
+    # start_set = r'(c:Tc + list< float >)'
     # simple_expr = ['a+b']
-    simple_expr = ['b = c']
+    # simple_expr = ['b = c']
     aux = maude.init()
     aux = maude.load('init.maude')
     constr_module = maude.getModule('CONSTR')
@@ -91,8 +24,10 @@ if __name__ == "__main__":
             mod = maude.getModule('tempmod')
             term = mod.parseTerm('c [nil]')
             f.write(f'{expr}{os.linesep}')
-            for result, nrew in term.srewrite(strat):
-                f.write(f'{result}{os.linesep} in {nrew} rewrites{os.linesep}')
+            # for result, nrew in term.srewrite(strat):
+            #     f.write(f'{result}{os.linesep} in {nrew} rewrites{os.linesep}')
             for result, nrew in term.srewrite(strat2):
                 f.write(f'{result}{os.linesep} in {nrew} rewrites{os.linesep}')
+                aux = parse_result(result)
+                print(aux)
             f.write(os.linesep)
--- statev2/maude_gen.py
+++ statev2/maude_gen.py
@@ -1,4 +1,8 @@
 import os
+import os.path
+import maude
+from statev2.transfer import *
+from united_specs import unitedspecs, op_equiv
 
 
 def vartype_generator(maxitems = 20):
@@ -30,3 +34,77 @@ def mod_generator(mod_name: str, constraints: str, indent=2, dump_to_file=False)
     if dump_to_file:
         open(mod_name + '.maude', 'w').write(maude_code)
     return maude_code
+
+
+def get_states(state_set: StateSet, expr: str) -> StateSet:
+    tree = ast.parse(expr)
+    tf = TransferFunc(state_set)
+    tf.visit(tree)
+    return tf.state_set
+
+
+def get_constraints_list(state_set: StateSet) -> list[AndConstraints]:
+    ll = []
+    for state in state_set:
+        ll.append(state.constraints)
+    return ll
+
+
+def get_constraints_string_from_states(state_set: StateSet) -> str:
+    print_str = ''
+    constr_list = get_constraints_list(state_set)
+    for constraint in constr_list:
+        retstr = str(constraint)
+        # for eq_in, eq_out in type_equiv.items():
+        #     retstr = retstr.replace(eq_in, eq_out)
+        print_str += f'({retstr}) \\/ {os.linesep}'
+    print_str = print_str[:(-4 - len(os.linesep))]
+    return print_str
+
+
+def output_expressions(expr_list: list[str], str_state_set: str,
+                       outc_file: str = 'out_constraints.txt', outs_file: str = 'out_states.txt'):
+    current = Translator.translate_state_set(str_state_set)
+    if os.path.exists(outc_file):
+        os.remove(outc_file)
+    if os.path.exists(outs_file):
+        os.remove(outs_file)
+    with open(outc_file, 'a') as f:
+        for expr in expr_list:
+            f.write(f'{'-' * (len(expr) + 4)}\n| {expr} |\n{'-' * (len(expr) + 4)}\n')
+            applied = get_states(current, expr)
+            print_str = get_constraints_string_from_states(applied)
+            f.write(print_str + '\n\n')
+    with open(outs_file, 'a') as f:
+        for expr in expr_list:
+            f.write(f'{'-' * (len(expr) + 4)}\n| {expr} |\n{'-' * (len(expr) + 4)}\n')
+            applied = get_states(current, expr)
+            print_str = ''
+            for state in applied:
+                retstr = str(state)
+                # for eq_in, eq_out in type_equiv.items():
+                #     retstr = retstr.replace(eq_in, eq_out)
+                print_str += f'({retstr}) \\/ \n'
+            f.write(print_str[:-5] + '\n\n')
+
+
+def dump_to_maude(expr: str, str_state_set: str, dump=False) -> str:
+    current = Translator.translate_state_set(str_state_set)
+    applied = get_states(current, expr)
+    applied = applied.replace_superclasses()
+    c_value = get_constraints_string_from_states(applied)
+    maude_input = mod_generator('tempmod', c_value, dump_to_file=dump)
+    return maude_input
+
+
+def parse_result(maude_result: maude.Term) -> list[Relation]:
+    relation_list = []
+    m_res = str(maude_result).replace('[nil]', '')
+    result_list = m_res.split('/\\')
+    for elem in result_list:
+        aux = elem.strip()
+        aux = aux.replace('(', '')
+        aux = aux.replace(')', '')
+        rel = Translator.translate_relation(aux)
+        relation_list.append(deepcopy(rel))
+    return relation_list
