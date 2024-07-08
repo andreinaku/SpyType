fa4a4e696d010646a6bd33376d3c596f8bf5ea10
Andrei Nacu <andreinaku@gmail.com>
2024-07-08 17:00:56 +0300

    save state

---
+++ benchmarks/monat_bench/assign_2.py
@@ -0,0 +1,12 @@
+import random
+
+if random.randint(0, 10) > 5:
+    a, b = [1, 2]
+elif random.randint(0, 10) > 5:
+    a, b = ['1', '2']
+elif random.randint(0, 10) > 5:
+    a, b = [1, 2.5]
+elif random.randint(0, 10) > 5:
+    a, b = [1, 2.5]
+else:
+    a, b = [None, None]
