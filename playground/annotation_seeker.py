6b9d26cc4f19b6dacf85b3a93e49ec2321497cf0
Andrei Nacu <andreinaku@gmail.com>
2025-02-10 08:23:43 +0200

    quicksave playground

--- playground/annotation_seeker.py
+++ playground/annotation_seeker.py
@@ -1,8 +1,88 @@
 import ast
 import astor
+import typing
 from typing import *
-from _typeshed import *
+# from _fakeshed import *
 import json
+import _ast
+
+from _fakeshed import (
+    AnyStr_co,
+    ConvertibleToFloat,
+    ConvertibleToInt,
+    FileDescriptorOrPath,
+    MaybeNone,
+    OpenBinaryMode,
+    OpenBinaryModeReading,
+    OpenBinaryModeUpdating,
+    OpenBinaryModeWriting,
+    OpenTextMode,
+    ReadableBuffer,
+    SupportsAdd,
+    SupportsAiter,
+    SupportsAnext,
+    SupportsDivMod,
+    SupportsFlush,
+    SupportsIter,
+    SupportsKeysAndGetItem,
+    SupportsLenAndGetItem,
+    SupportsNext,
+    SupportsRAdd,
+    SupportsRDivMod,
+    SupportsRichComparison,
+    SupportsRichComparisonT,
+    SupportsWrite
+)
+
+
+class ProtocolSeeker(ast.NodeVisitor):
+    def __init__(self, tree: ast.AST):
+        self.tree = tree
+        self.protocols = []
+        self.parent_name = "Protocol"
+
+    def visit_ClassDef(self, node: ast.ClassDef):
+        if len(node.bases) < 1:
+            return
+        str_bases = []
+        for base in node.bases:
+            if not isinstance(base, ast.Name):
+                continue
+            str_bases.append(base.id)
+        if self.parent_name not in str_bases:
+            return
+        def_str = astor.to_source(node).strip()
+        exec(def_str, globals())
+        print(f"evalled {def_str}")
+    
+    def gather_protocols(self):
+        self.visit(self.tree)
+
+
+class TypeVarSeeker(ast.NodeVisitor):
+    def __init__(self, tree: ast.AST):
+        self.tree = tree
+        self.calls = []
+        self.funcname = "TypeVar"
+    
+    def visit_Assign(self, node: ast.Assign):
+        if len(node.targets) > 1:
+            return
+        target = node.targets[0]
+        if not isinstance(target, ast.Name):
+            return
+        if not isinstance(node.value, ast.Call):
+            return
+        if not isinstance(node.value.func, ast.Name) and node.value.func.id != self.funcname:
+            return
+        if len(node.value.args) < 1 and not isinstance(node.value.args[0], ast.Constant):
+            return
+        assign_str = astor.to_source(node).strip()
+        exec(assign_str, globals())
+        print(f"execced {assign_str}")
+
+    def gather_typevars(self):
+        self.visit(self.tree)
 
 
 class AnnotationSeeker(ast.NodeVisitor):
@@ -19,7 +99,7 @@ class AnnotationSeeker(ast.NodeVisitor):
                 argstr = astor.to_source(arg.annotation).strip()
                 eval(argstr)
                 self.goodies.append(argstr)
-            except:
+            except Exception as e:
                 # print(f"not evalled {argstr}")
                 self.problems.append(argstr)
 
@@ -33,12 +113,16 @@ class AnnotationSeeker(ast.NodeVisitor):
 def seek_from_stubs(fname: str) -> list[str]:
     with open(fname, 'r') as f:
         tree = ast.parse(f.read())
+    ProtocolSeeker(tree).gather_protocols()
+    TypeVarSeeker(tree).gather_typevars()
     aux = AnnotationSeeker(tree).collect()
     return aux
 
 
+
 if __name__ == "__main__":
-    fname = "/Users/andrei/work/doctorat/SpyType/sheds/builtins.pyi"
+    fname = "/Users/andrei/work/doctorat/SpyType/playground/fakeins.pyi"
+    eval("SupportsNext")
     goodlist, badlist = seek_from_stubs(fname)
     print(f"good = {len(goodlist)} items\nbad = {len(badlist)} items")
     with open("badseeks.json", "w") as f:
