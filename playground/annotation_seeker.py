73ec91dfe60e2d987ac4e00ca577f04f57d6f2f1
Andrei Nacu <andreinaku@gmail.com>
2025-02-16 18:57:03 +0200

    quicksave, with stats

--- playground/annotation_seeker.py
+++ playground/annotation_seeker.py
@@ -9,6 +9,7 @@ import _ast
 from copy import deepcopy
 import types
 from typing_test import create_basetype
+from collections import defaultdict
 
 
 from _fakeshed import (
@@ -44,8 +45,12 @@ annotation_exceptions = {
     "_SupportsSomeKindOfPow": "_SupportsSomeKindOfPow: TypeAlias = _SupportsPow2[Any, Any] | _SupportsPow3NoneOnly[Any, Any] | _SupportsPow3[Any, Any, Any]",
 }
 
-selftypes = {"Self", "_typeshed.Self"}
+selftypes = {"Self", "_typeshed.Self", "type[Self]", "type[_typeshed.Self]"}
 skips = ["Callable"]
+
+visited_nodes = []
+class_stats = dict()
+class_stats = {'indie': {'translatable': 0, 'total': 0}}
 
 
 class ProtocolSeeker(ast.NodeVisitor):
@@ -106,6 +111,9 @@ class TypeAliasSeeker(ast.NodeVisitor):
         self.tree = tree
 
     def visit_Assign(self, node: ast.Assign):
+        if node in visited_nodes:
+            return
+        visited_nodes.append(node)
         if not isinstance(node.targets[0], ast.Name) and node.targets[0] not in annotation_exceptions:
             return
         node_src = astor.to_source(node).strip()
@@ -115,6 +123,9 @@ class TypeAliasSeeker(ast.NodeVisitor):
             print(f"possibly recursive: {node_src}")
 
     def visit_AnnAssign(self, node: ast.AnnAssign):
+        if node in visited_nodes:
+            return
+        visited_nodes.append(node)
         if not hasattr(node.annotation, "id"):
             return
         if node.annotation.id != "TypeAlias":
@@ -135,42 +146,61 @@ class AnnotationSeeker(ast.NodeVisitor):
         self.problems = []
         self.goodies = []
         self.current_class = None
-
+    
     def visit_ClassDef(self, node: ast.ClassDef):
+        if node in visited_nodes:
+            return
+        visited_nodes.append(node)
         self.current_class = node
+        class_stats[self.current_class.name] = {'translatable': 0, 'total': 0}
         self.generic_visit(node)
         self.current_class = None
 
     def visit_FunctionDef(self, node: ast.FunctionDef):
-        for arg in node.args.args:
-            if not hasattr(arg, "annotation") or arg.annotation is None:
-                continue
-            try:
-                argstr = astor.to_source(arg.annotation).strip()
-                if argstr in selftypes:  # the type is the class name, for self-ish annotations
-                    argstr = self.current_class.name
-                for skip in skips:
-                    if skip in argstr:
-                        raise TypeError("(skipped)")
-                eval(argstr)
-                self.goodies.append(argstr)
-            except Exception as e:
-                # print(f"not evalled {argstr}")
-                self.problems.append(f"{argstr}: {str(e)}")
+        if node in visited_nodes:
+            return
+        if self.current_class is None:
+            stats_key = 'indie'
+        else:
+            stats_key = self.current_class.name
+        class_stats[stats_key]['total'] += 1            
+        visited_nodes.append(node)
+        arg_lists = [node.args.args, node.args.posonlyargs, node.args.kwonlyargs]
+        is_translatable = True
+        for arg_list in arg_lists:
+            for arg in arg_list:
+                if not hasattr(arg, "annotation") or arg.annotation is None:
+                    continue
+                try:
+                    argstr = astor.to_source(arg.annotation).strip()
+                    if argstr in selftypes:  # the type is the class name, for self-ish annotations
+                        argstr = self.current_class.name
+                    for skip in skips:
+                        if skip in argstr:
+                            raise TypeError("(skipped)")
+                    evalled = eval(argstr)
+                    auxbt = create_basetype(evalled)
+                    self.goodies.append(argstr)
+                except Exception as e:
+                    self.problems.append(f"{argstr}: {str(e)}")
+                    is_translatable = False
+        if is_translatable:
+            class_stats[stats_key]['translatable'] += 1
+
 
     def collect(self) -> list[str]:
         self.visit(self.tree)
-        # self.goodies = list(set(self.goodies))
-        # self.problems = list(set(self.problems))
         return self.goodies, self.problems
-
+    
 
 def seek_from_stubs(fname: str) -> list[str]:
     with open(fname, 'r') as f:
         tree = ast.parse(f.read())
     good_annotations = []
     bad_annotations = []
-    for _node in tree.body:    
+    for _node in tree.body:
+        if _node in visited_nodes:
+            continue
         TypeAliasSeeker(_node).gather_typealiases()
         ProtocolSeeker(_node).gather_protocols()
         TypeVarSeeker(_node).gather_typevars()
@@ -182,15 +212,33 @@ def seek_from_stubs(fname: str) -> list[str]:
     return good_annotations, bad_annotations
 
 
+def check_stats():
+    for classname, stats in class_stats.items():
+        if stats['total'] < stats['translatable']:
+            raise RuntimeError('why?')
+
+
+def parse_stats():
+    check_stats()
+    trans = 0
+    total = 0
+    for classname, stats in class_stats.items():
+        total += stats['total']
+        trans += stats['translatable']
+    percent = trans/total * 100
+    print(f"{percent:.2f}% function specifications are translatable")
+
+
 if __name__ == "__main__":
     fname = "playground/fakeins.pyi"
     goodlist, badlist = seek_from_stubs(fname)
+    parse_stats()
     print(f"good = {len(goodlist)} items\nbad = {len(badlist)} items")
     with open("badseeks.json", "w") as f:
         json.dump(badlist, f, indent=4)
-    good_dict = dict()
-    for goodstr in goodlist:
-        _good = create_basetype(eval(goodstr))
-        good_dict[goodstr] = str(_good)
-    with open("goodtypes.json", "w") as f:
-        json.dump(good_dict, f, indent=4)
+    # good_dict = dict()
+    # for goodstr in goodlist:
+    #     _good = create_basetype(eval(goodstr))
+    #     good_dict[goodstr] = str(_good)
+    # with open("goodtypes.json", "w") as f:
+    #     json.dump(good_dict, f, indent=4)
